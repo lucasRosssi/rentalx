0504390181d8442da210da26e9b7481834509fab
lucasRosssi <lucasrm2305@gmail.com>
2022-05-18 19:53:18 -0300

    created main server file

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import { router } from "./routes";
+import swaggerFile from "./swagger.json";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.listen(3333, () =>
+	console.log("\x1b[32m%s\x1b[0m", "\n ✔ Server is running!\n")
+);
